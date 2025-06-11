f5d0b16cd046a88a44e7b4715a0f840595c32678
lelz111 <slleonard86@gmail.com>
2025-06-11 14:25:27 +0700

    Practical Example-Control Flow

--- exercise/day  1/Control Flow/src/Main.java
+++ exercise/day  1/Control Flow/src/Main.java
@@ -3,6 +3,27 @@
 public class Main {
     public static void main(String[] args) {
             //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+//        Practical Example
+        //Print all even numbers between 1 and 20
+
+
+
+        for (int i = 1; i <= 20; i++){
+
+        if (i % 2 == 0){
+            System.out.println(i);
+        }
+        }
+        //Calculate the factorial of a given number
+        int number = 10;
+        int factorial = 1;
+        for (int k = 1; k <= number; k++){
+            factorial *= k;
+        }
+        System.out.println("Faktorial dari " + number + " adalah: " + factorial);
+
+
+
 //    for (int i = 1; i <= 10; i++){
 //        // to see how IntelliJ IDEA suggests fixing it.
 //     if(i <= 3){
@@ -29,16 +50,16 @@ public class Main {
 //    }
 
 //        While LOOP
-        int i = 1;
-        while(i < 10){
-            System.out.println("Ha");
-            i++;
-        };
-
-        do{
-            System.out.println("Haha");
-            i++;
-        }while (i > 15);
+//        int i = 1;
+//        while(i < 10){
+//            System.out.println("Ha");
+//            i++;
+//        };
+//
+//        do{
+//            System.out.println("Haha");
+//            i++;
+//        }while (i > 15);
 
     }
 }
