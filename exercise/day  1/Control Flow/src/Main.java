f3815a1b3a15ab358eeb2700e7886bd540d36e95
lelz111 <slleonard86@gmail.com>
2025-06-11 14:04:50 +0700

    While dan Do While

--- exercise/day  1/Control Flow/src/Main.java
+++ exercise/day  1/Control Flow/src/Main.java
@@ -3,30 +3,42 @@
 public class Main {
     public static void main(String[] args) {
             //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    for (int i = 1; i <= 10; i++){
-        // to see how IntelliJ IDEA suggests fixing it.
-     if(i <= 3){
-         System.out.println(i + " Ini Dibawah 4");
-     }else if(i <= 6){
-         System.out.println(i + " Ini Dibawah 7");
-     }else if(i < 10){
-        System.out.println(i + " Ini Dibawah 10");
-     }else{
-         System.out.println(i + " Ini adalah 10");
-     }
-        }
+//    for (int i = 1; i <= 10; i++){
+//        // to see how IntelliJ IDEA suggests fixing it.
+//     if(i <= 3){
+//         System.out.println(i + " Ini Dibawah 4");
+//     }else if(i <= 6){
+//         System.out.println(i + " Ini Dibawah 7");
+//     }else if(i < 10){
+//        System.out.println(i + " Ini Dibawah 10");
+//     }else{
+//         System.out.println(i + " Ini adalah 10");
+//     }
+//        }
+//
+//    for (int j = 1; j <= 3;j++){
+//
+//     String angka;
+//     switch (j){
+//         case 1: angka = "satu"; break;
+//         case 2: angka = "dua"; break;
+//         case 3: angka = "tiga"; break;
+//         default: angka = "invalid name"; break;
+//     }
+//     System.out.println(angka);
+//    }
 
-    for (int j = 1; j <= 3;j++){
+//        While LOOP
+        int i = 1;
+        while(i < 10){
+            System.out.println("Ha");
+            i++;
+        };
 
-     String angka;
-     switch (j){
-         case 1: angka = "satu"; break;
-         case 2: angka = "dua"; break;
-         case 3: angka = "tiga"; break;
-         default: angka = "invalid name"; break;
-     }
-     System.out.println(angka);
-    }
+        do{
+            System.out.println("Haha");
+            i++;
+        }while (i > 15);
 
     }
 }
