5344f5bb1503f9ad36614a80f3b153d0ea926a14
lelz111 <slleonard86@gmail.com>
2025-06-11 13:29:57 +0700

    Control Flow

---
+++ exercise/day  1/Control Flow/src/Main.java
@@ -0,0 +1,32 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+            //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+    for (int i = 1; i <= 10; i++){
+        // to see how IntelliJ IDEA suggests fixing it.
+     if(i <= 3){
+         System.out.println(i + " Ini Dibawah 4");
+     }else if(i <= 6){
+         System.out.println(i + " Ini Dibawah 7");
+     }else if(i < 10){
+        System.out.println(i + " Ini Dibawah 10");
+     }else{
+         System.out.println(i + " Ini adalah 10");
+     }
+        }
+
+    for (int j = 1; j <= 3;j++){
+
+     String angka;
+     switch (j){
+         case 1: angka = "satu"; break;
+         case 2: angka = "dua"; break;
+         case 3: angka = "tiga"; break;
+         default: angka = "invalid name"; break;
+     }
+     System.out.println(angka);
+    }
+
+    }
+}
