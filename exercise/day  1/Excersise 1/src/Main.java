e1b852671d843be9e811bea5f3913d8fab26d3df
lelz111 <slleonard86@gmail.com>
2025-06-11 12:01:03 +0700

    Update Operators

--- exercise/day  1/Excersise 1/src/Main.java
+++ exercise/day  1/Excersise 1/src/Main.java
@@ -5,26 +5,26 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
 //        System.out.println("Hello, World!");
-        long mylong = 10;
-        int myint = 10;
-        float myfloat = 10.55f;
-        double mydouble = 10.55;
-        char name = 'L';
-        boolean valid = true;
-        String nama = "Leon";
-        int[] numbers = {1,2,3,4};
-
-        System.out.println("long: " + mylong);
-        System.out.println("int: " + myint);
-        System.out.println("float: " + myfloat);
-        System.out.println("double: " + mydouble);
-        System.out.println("char: " + name);
-        System.out.println("boolean: " + valid);
-        System.out.println("String: " + nama);
-        System.out.println("Array: ");
-        for (int num : numbers) {
-            System.out.printf(num + " ");
-        }
+//        long mylong = 10;
+//        int myint = 10;
+//        float myfloat = 10.55f;
+//        double mydouble = 10.55;
+//        char name = 'L';
+//        boolean valid = true;
+//        String nama = "Leon";
+//        int[] numbers = {1,2,3,4};
+//
+//        System.out.println("long: " + mylong);
+//        System.out.println("int: " + myint);
+//        System.out.println("float: " + myfloat);
+//        System.out.println("double: " + mydouble);
+//        System.out.println("char: " + name);
+//        System.out.println("boolean: " + valid);
+//        System.out.println("String: " + nama);
+//        System.out.println("Array: ");
+//        for (int num : numbers) {
+//            System.out.printf(num + " ");
+//        }
 
 //        System.out.printf("long: " + mylong);
 //        System.out.printf("int: " + myint);
@@ -48,6 +48,25 @@ public class Main {
 //                        "String: " + nama + ", " +
 //                        "Array: " + arrayString.toString().trim()
 //        );
+
+//        OPERATORS
+
+        int mul = 10*5;
+        int div = 3000/6;
+
+        boolean istrue = 0 < 3;
+        boolean isfalse = 6 <= 3;
+
+        boolean result = (50 >= 5) && (5 > 3);
+        boolean result2 = (50 >= 5) || (5 > 3);
+
+        System.out.println(mul);
+        System.out.println(div);
+        System.out.println(istrue);
+        System.out.println(isfalse);
+        System.out.println(result);
+        System.out.println(result2);
+
 
 
 
