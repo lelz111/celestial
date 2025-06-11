cc647fe24d467f97522d20abf9325f69d5125ff5
lelz111 <slleonard86@gmail.com>
2025-06-11 11:50:50 +0700

    Data Types

--- exercise/day  1/Excersise 1/src/Main.java
+++ exercise/day  1/Excersise 1/src/Main.java
@@ -4,7 +4,53 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello, World!");
+//        System.out.println("Hello, World!");
+        long mylong = 10;
+        int myint = 10;
+        float myfloat = 10.55f;
+        double mydouble = 10.55;
+        char name = 'L';
+        boolean valid = true;
+        String nama = "Leon";
+        int[] numbers = {1,2,3,4};
+
+        System.out.println("long: " + mylong);
+        System.out.println("int: " + myint);
+        System.out.println("float: " + myfloat);
+        System.out.println("double: " + mydouble);
+        System.out.println("char: " + name);
+        System.out.println("boolean: " + valid);
+        System.out.println("String: " + nama);
+        System.out.println("Array: ");
+        for (int num : numbers) {
+            System.out.printf(num + " ");
+        }
+
+//        System.out.printf("long: " + mylong);
+//        System.out.printf("int: " + myint);
+//        System.out.printf("float: " + myfloat);
+//        System.out.printf("double: " + mydouble);
+//        System.out.printf("char: " + name);
+//        System.out.printf("boolean: " + valid);
+//        System.out.printf("String: " + nama);
+
+//        StringBuilder arrayString = new StringBuilder();
+//        for (int num : numbers) {
+//            arrayString.append(num).append(" ");
+//        }
+//        System.out.println(
+//                "long: " + mylong + ", " +
+//                        "int: " + myint + ", " +
+//                        "float: " + myfloat + ", " +
+//                        "double: " + mydouble + ", " +
+//                        "char: " + name + ", " +
+//                        "boolean: " + valid + ", " +
+//                        "String: " + nama + ", " +
+//                        "Array: " + arrayString.toString().trim()
+//        );
+
+
+
 
     }
 }
